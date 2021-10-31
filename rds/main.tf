5e9e692adee689df0896e61ea58c21d9964a1d26
shukubota <kubotashu0421@gmail.com>
2021-10-31 23:15:47 +0900

    databaseの設定

--- rds/main.tf
+++ rds/main.tf
@@ -3,19 +3,19 @@ provider "aws" {
 }
 
 variable "username" {
-  type = string
-  default = "speaking"
+//  type = string
+//  default = ""
 }
 variable "password" {
-  type = string
-  default = "speakingpass"
+//  type = string
+//  default = ""
 }
 
 # RDS
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
   engine               = "postgres"
-  identifier = "speakingCamp"
+  identifier = "speaking-camp"
   engine_version = "12.7"
   instance_class       = "db.t3.micro"
   name                 = "speakingCamp"
